163006acde5ecd533b0d8076e0b5aac947405107
sumit gawande <gawandesumit97@gmail.com>
2020-10-29 12:29:35 +0530

    Updates

---
+++ precache-manifest.4480dd317b65da25022b7fcc1086c47d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3919d36312a358a1f421d42b0c0546e9",
+    "url": "/sample-website/index.html"
+  },
+  {
+    "revision": "4e4aeabc8595166b6f79",
+    "url": "/sample-website/static/css/main.8e1739c7.chunk.css"
+  },
+  {
+    "revision": "cdc507348bc2633b529a",
+    "url": "/sample-website/static/js/2.bd9f207a.chunk.js"
+  },
+  {
+    "revision": "038137378776d3782021381009b8dd7f",
+    "url": "/sample-website/static/js/2.bd9f207a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4aeabc8595166b6f79",
+    "url": "/sample-website/static/js/main.7e95c791.chunk.js"
+  },
+  {
+    "revision": "a8f236f8910262e47a8d",
+    "url": "/sample-website/static/js/runtime-main.1b43e7b3.js"
+  }
+]);
